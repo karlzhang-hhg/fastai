73fd5761734c2d7a40e4a76339f0cb523a4d1bfb
NU-crunch <zkg@u.northwestern.edu>
2019-11-01 12:07:31 -0500

    mc

--- fastai/basic_train.py
+++ fastai/basic_train.py
@@ -23,43 +23,43 @@ def loss_batch(model:nn.Module, xb:Tensor, yb:Tensor, loss_func:OptLossFunc=None
     cb_handler = ifnone(cb_handler, CallbackHandler())
     if not is_listy(xb): xb = [xb]
     if not is_listy(yb): yb = [yb]
-    print(xb[1][:2], yb[0][:2])
-    print("The batch mean and std are {}, {}.\n".format(xb[1].mean(axis=0),xb[1].std(axis=0)))
+    # print(xb[1][:2], yb[0][:2])
+    # print("The batch mean and std are {}, {}.\n".format(xb[1].mean(axis=0),xb[1].std(axis=0)))
     out = model(*xb)
-    print("The output of model is {} with shape {}.\n".format(out[:2],out.shape))
+    # print("The output of model is {} with shape {}.\n".format(out[:2],out.shape))
     out = cb_handler.on_loss_begin(out)
-    print("The output of model is {} with shape {}.\n".format(out[:2],out.shape))
+    # print("The output of model is {} with shape {}.\n".format(out[:2],out.shape))
 
     if not loss_func: return to_detach(out), to_detach(yb[0])
     loss = loss_func(out.view(-1,), *yb)
-    expanded_input_1, expanded_target_1 = torch.broadcast_tensors(out, yb[0])
-    expanded_input_2, expanded_target_2 = torch.broadcast_tensors(out, *yb)
-    expanded_input_3, expanded_target_3 = torch.broadcast_tensors(out.view(-1,), *yb)
-    print("The shape of all broadcasted input and target are {} {} {} {} {} {}.".format(expanded_input_1.shape, expanded_target_1.shape,expanded_input_2.shape, expanded_target_2.shape,expanded_input_3.shape, expanded_target_3.shape))
-    print("The first 2 rows of all broadcasted input and target are {} {} {} {} {} {}.".format(expanded_input_1[:2], expanded_target_1[:2],expanded_input_2[:2], expanded_target_2[:2],expanded_input_3[:2], expanded_target_3[:2]))
-    print("Self calculating gradient:\n")
+    # expanded_input_1, expanded_target_1 = torch.broadcast_tensors(out, yb[0])
+    # expanded_input_2, expanded_target_2 = torch.broadcast_tensors(out, *yb)
+    # expanded_input_3, expanded_target_3 = torch.broadcast_tensors(out.view(-1,), *yb)
+    # print("The shape of all broadcasted input and target are {} {} {} {} {} {}.".format(expanded_input_1.shape, expanded_target_1.shape,expanded_input_2.shape, expanded_target_2.shape,expanded_input_3.shape, expanded_target_3.shape))
+    # print("The first 2 rows of all broadcasted input and target are {} {} {} {} {} {}.".format(expanded_input_1[:2], expanded_target_1[:2],expanded_input_2[:2], expanded_target_2[:2],expanded_input_3[:2], expanded_target_3[:2]))
+    # print("Self calculating gradient:\n")
     ls_params = [p for p in model.parameters()]
-    print(loss_func, type(loss_func), torch.matmul(xb[1], ls_params[0].data.view(-1,1))+ls_params[1].data-out)
-    resi = (yb[0].view(-1,1)-out)
-    print(out.shape, yb[0].shape, resi.shape, xb[1].shape, (resi**2).mean(), loss, out.numel())
-    print(2*resi.float().mean(axis=0), 2*(resi * xb[1]).float().mean(axis=0), 2*(resi * (xb[1]-xb[1].mean(0))/xb[1].std(0)).float().mean(axis=0))
+    # print(loss_func, type(loss_func), torch.matmul(xb[1], ls_params[0].data.view(-1,1))+ls_params[1].data-out)
+    # resi = (yb[0].view(-1,1)-out)
+    # print(out.shape, yb[0].shape, resi.shape, xb[1].shape, (resi**2).mean(), loss, out.numel())
+    print(2*resi.float().mean(axis=0), 2*(resi * xb[1]).float().mean(axis=0))
 
     if opt is not None:
         loss,skip_bwd = cb_handler.on_backward_begin(loss)
         if not skip_bwd:                     loss.backward()
-        print("print gradient (before):")
-        for group in opt.param_groups:
-            for p in group['params']:
-                if p is not None:
-                    print(p.grad.data)
-                    print("-------------")
+        # print("print gradient (before):")
+        # for group in opt.param_groups:
+        #     for p in group['params']:
+        #         if p is not None:
+        #             print(p.grad.data)
+        #             print("-------------")
         if not cb_handler.on_backward_end(): opt.step()
-        print("print gradient (after):")
-        for group in opt.param_groups:
-            for p in group['params']:
-                if p is not None:
-                    print(p.grad.data)
-                    print("-------------")
+        # print("print gradient (after):")
+        # for group in opt.param_groups:
+        #     for p in group['params']:
+        #         if p is not None:
+        #             print(p.grad.data)
+        #             print("-------------")
         if not cb_handler.on_step_end():     opt.zero_grad()
     
     # print(loss.detach().cpu())
